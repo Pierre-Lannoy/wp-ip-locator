27ed4db8856acc0e7939dae91bfcf17ce706053f
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-03-30 12:43:07 +0200

    Adds language detection.

---
+++ includes/api/class-lang.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * IP Locator lang detector.
+ *
+ * @package API
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.3.0
+ */
+
+namespace IPLocator;
+
+use IPLocator\System\Environment;
+use IPLocator\System\Logger;
+use IPLocator\System\Option;
+use IPLocator\System\Hosting;
+use IPLocator\System\IP;
+use IPLocator\System\Cache;
+use IPLocator\Plugin\Feature\Schema;
+use IPLocator\System\L10n;
+
+/**
+ * IP Locator country lang class.
+ *
+ * This class defines all code necessary to detect lang.
+ *
+ * @package API
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+class Lang {
+
+	/**
+	 * The detected country code (ISO 3166-1 alpha-2).
+	 *
+	 * @since  1.0.0
+	 * @var    string    $cc    Maintains the country code.
+	 */
+	private $cc = '00';
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct( $cc ) {
+		$this->cc = $cc;
+	}
+
+	/**
+	 * Get the country main language.
+	 *
+	 * @return string   The main language if found, '' otherwise.
+	 * @since 1.0.0
+	 */
+	public function code() {
+		return L10n::get_main_lang_code( $this->cc );
+	}
+
+	/**
+	 * Get the country main language name.
+	 *
+	 * @param  string $locale   Optional. The locale.
+	 * @return string           The country main language name, localized if possible.
+	 * @since 1.0.0
+	 */
+	public function name( $locale = null ) {
+		return L10n::get_main_lang_name( $this->cc, self::code(), $locale );
+	}
+
+}
+
+
