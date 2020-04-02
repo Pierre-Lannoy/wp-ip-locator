dec2876076d367d8697084c93dddd66b58c712a5
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-04-02 09:23:01 +0200

    Ready to release 1.0.0 on wp.org.

--- init.php
+++ init.php
@@ -12,11 +12,11 @@ define( 'IPLOCATOR_PRODUCT_URL', 'https://github.com/Pierre-Lannoy/wp-ip-locator
 define( 'IPLOCATOR_PRODUCT_SHORTNAME', 'IP Locator' );
 define( 'IPLOCATOR_PRODUCT_ABBREVIATION', 'iplocator' );
 define( 'IPLOCATOR_SLUG', 'ip-locator' );
-define( 'IPLOCATOR_VERSION', '1.0.0-rc3' );
+define( 'IPLOCATOR_VERSION', '1.0.0' );
 define( 'IPLOCATOR_CODENAME', '"-"' );
 
 define( 'IPLOCATOR_INIT_TIMEOUT', 1 * HOUR_IN_SECONDS );
 define( 'IPLOCATOR_UPDATE_TIMEOUT', 1 * HOUR_IN_SECONDS );
-define( 'IPLOCATOR_UPDATE_CYCLE', 3 ); // Update cycle in days
+define( 'IPLOCATOR_UPDATE_CYCLE', 4 ); // Update cycle in days
 
 define( 'IPLOCATOR_CDN_AVAILABLE', true );
