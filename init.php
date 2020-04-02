6f9ab2272dce85c96ae57c65602e2cb309817919
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-04-02 18:35:53 +0200

    Ready to release 1.0.1 on wp.org - the initial release, finally.

--- init.php
+++ init.php
@@ -12,7 +12,7 @@ define( 'IPLOCATOR_PRODUCT_URL', 'https://github.com/Pierre-Lannoy/wp-ip-locator
 define( 'IPLOCATOR_PRODUCT_SHORTNAME', 'IP Locator' );
 define( 'IPLOCATOR_PRODUCT_ABBREVIATION', 'iplocator' );
 define( 'IPLOCATOR_SLUG', 'ip-locator' );
-define( 'IPLOCATOR_VERSION', '1.0.0' );
+define( 'IPLOCATOR_VERSION', '1.0.1' );
 define( 'IPLOCATOR_CODENAME', '"-"' );
 
 define( 'IPLOCATOR_INIT_TIMEOUT', 1 * HOUR_IN_SECONDS );
