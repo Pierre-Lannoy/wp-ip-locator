5c1d7c08371e93eb23b9a0e81adc7803b18366a0
Pierre Lannoy <pierre@lannoy.fr>
2021-06-04 19:37:53 +0200

    Uses new SDK detection features.

--- admin/class-ip-locator-admin.php
+++ admin/class-ip-locator-admin.php
@@ -388,9 +388,9 @@ class IP_Locator_Admin {
 		}
 		
 		register_setting( 'iplocator_plugin_features_section', 'iplocator_plugin_features_autoupdate' );
-		if ( defined( 'DECALOG_VERSION' ) ) {
+		if ( \DecaLog\Engine::isDecalogActivated() ) {
 			$help  = '<img style="width:16px;vertical-align:text-bottom;" src="' . \Feather\Icons::get_base64( 'thumbs-up', 'none', '#00C800' ) . '" />&nbsp;';
-			$help .= sprintf( esc_html__('Your site is currently using %s.', 'ip-locator' ), '<em>DecaLog v' . DECALOG_VERSION .'</em>' );
+			$help .= sprintf( esc_html__('Your site is currently using %s.', 'ip-locator' ), '<em>' . \DecaLog\Engine::getVersionString() .'</em>' );
 		} else {
 			$help  = '<img style="width:16px;vertical-align:text-bottom;" src="' . \Feather\Icons::get_base64( 'alert-triangle', 'none', '#FF8C00' ) . '" />&nbsp;';
 			$help .= sprintf( esc_html__('Your site does not use any logging plugin. To log all events triggered in IP Locator, I recommend you to install the excellent (and free) %s. But it is not mandatory.', 'ip-locator' ), '<a href="https://wordpress.org/plugins/decalog/">DecaLog</a>' );
