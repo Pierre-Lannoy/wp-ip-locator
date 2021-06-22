a1a2198238ce62f8bdd0c519262acd453516ae94
Pierre Lannoy <pierre@lannoy.fr>
2021-06-22 14:15:59 +0200

    Ready to release 3.1.1 on wp.org.

--- includes/plugin/class-core.php
+++ includes/plugin/class-core.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The file that defines the core plugin class
+ * The file that defines the core  plugin class
  *
  * A class definition that includes attributes and functions used across both the
  * public-facing side of the site and the admin area.
